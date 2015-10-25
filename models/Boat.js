6e0fc648039854130de4ad101ab86f14f2235f18
pa <paxapy@gmail.com>
2015-10-25 20:18:38 +0000

    actual boat model

---
+++ models/Boat.js
@@ -0,0 +1,28 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+
+var Boat = new keystone.List('Boat', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true }
+});
+
+Boat.add({
+	title: { type: String, required: true },
+	price: { type: Types.Money, currency: 'ru' },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	image: { type: Types.CloudinaryImage },
+	content: {
+		brief: { type: Types.Html, wysiwyg: true, height: 150 },
+		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+	},
+});
+
+Boat.schema.virtual('content.full').get(function() {
+	return this.content.extended || this.content.brief;
+});
+
+
+
+Boat.defaultColumns = 'title, price, state|20%';
+Boat.register();
