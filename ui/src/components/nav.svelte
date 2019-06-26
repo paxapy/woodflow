03ba5495ed3325f79cfcc372860aa8bc402fbbea
paxapy <paxapy@gmail.com>
2019-06-26 12:53:35 +0300

    ui/src: make home img more responsive, fix fonts setup, fix logo

--- ui/src/components/nav.svelte
+++ ui/src/components/nav.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav>
-	<img title="woodflow" src="img/icon.png">
+	<img alt="woodflow" src="img/icon.png">
 	<a class='{segment === undefined ? "selected" : ""}' href='.'>мастерская</a>
 	<a class='{segment === "about" ? "selected" : ""}' href='about'>лодки и каноэ</a>
 	<a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a>
